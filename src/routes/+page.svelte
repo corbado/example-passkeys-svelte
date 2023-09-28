c0edbcb48f3179d4a0ad884591867bebb788e068
Vincent Delitz <vincent.delitz@corbado.com>
2023-09-28 09:26:02 +0200

    updated profile and npm package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {PUBLIC_CORBADO_PROJECT_ID} from '$env/static/public';
+
+    import '@corbado/webcomponent/pkg/auth_cui.css'
+    import '@corbado/webcomponent'
+</script>
+<corbado-auth
+        project-id={PUBLIC_CORBADO_PROJECT_ID}
+        conditional="yes"
+>
+    <input
+            name="username"
+            id="corbado-username"
+            required
+            autocomplete="webauthn"
+    />
+</corbado-auth>
