46b9f6fbb54dd5b25036f3ccb97ed56906f2703a
Lukas Kratzel <lukas.kratzel2004@gmail.com>
2024-04-25 18:00:21 +0200

    migrate to web-js and typescript

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Corbado from "@corbado/web-js";
+    import {onMount} from "svelte";
+    const PROJECT_ID = import.meta.env.VITE_CORBADO_PROJECT_ID;
+
+    let isInitialized = false;
+    onMount(async () => {
+        await Corbado.load({
+            //@ts-ignore
+            projectId: PROJECT_ID,
+            darkMode: 'off'
+        });
+        isInitialized = true; // Set to true once Corbado is initialized
+    });
+</script>
+
+<div>
+    {#if isInitialized}
+        <slot></slot>
+    {/if}
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import preprocess from 'svelte-preprocess';
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-	],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	ssr: {
-		noExternal: ['@corbado/webcomponent']
 	}
 };
 
